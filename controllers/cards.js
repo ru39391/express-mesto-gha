6df9d39304a7dfa148ebfc677dbe9fd4b339df8f
ru39391 <dev@39391.ru>
2022-09-19 23:41:31 +0300

    feature/исправить названия передаваемых параметров

--- controllers/cards.js
+++ controllers/cards.js
@@ -8,15 +8,15 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const owner = req.user._id;
-  const { name, link, likes, createdAt } = req.body;
+  const { name, link } = req.body;
 
-  Card.create({ name, link, owner, likes, createdAt })
+  Card.create({ name, link, owner })
     .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: err.message }));
 };
 
 module.exports.removeCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.id)
+  Card.findByIdAndRemove(req.params.cardId)
     .then(card => res.send({ data: card }))
     .catch(err => res.status(500).send({ message: err.message }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUser = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then(user => res.send({ data: user }))
     .catch(err => res.status(500).send({ message: err.message }));
 };
