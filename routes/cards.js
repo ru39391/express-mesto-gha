59d5285607a9a6c5eeda233e8126fe0a640e9edf
ru39391 <dev@39391.ru>
2022-09-27 12:02:38 +0300

    feature/настроить валидацию передаваемых в запрос данных, pt. 1

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, createCard, removeCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+}), createCard);
 router.delete('/:cardId', removeCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
